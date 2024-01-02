893f26d33ec87ee1bf0f036881ba619a2a5b1f1a
vanditakambiri <vandita.kambiri@yahoo.com>
2024-01-01 17:10:46 -0700

    giving details of my age

---
+++ src/Javaproject.java
@@ -0,0 +1,10 @@
+
+public class Javaproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myage = 33;
+System.out.println("Hello Eclipse");
+		System.out.println("My Age is:" + myage);
+}
+}
